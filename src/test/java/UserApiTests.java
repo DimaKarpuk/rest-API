dfac6156bba5ee4e503400a5e5d3ce711c053a20
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-06-20 18:41:30 +0300

    added allure

--- src/test/java/UserApiTests.java
+++ src/test/java/UserApiTests.java
@@ -4,10 +4,13 @@ import models.UserApiModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class UserApiTests extends TestBase {
     @DisplayName("Успешная регистрация пользователя")
@@ -17,21 +20,26 @@ public class UserApiTests extends TestBase {
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("pistol");
 
-        ResponseBodyModel response = given()
-                .body(authData)
-                .contentType(JSON)
-                .log().uri()
-                .log().all()
-                .when()
-                .post("/register")
-                .then()
-                .log().status()
-                .log().body()
-                .log().all()
-                .statusCode(200)
-                .extract().as(ResponseBodyModel.class);
-        assertEquals(4,response.getId());
-        assertEquals("QpwL5tke4Pnpja7X4",response.getToken());
+        ResponseBodyModel response = step("Make request", () ->{
+           return given()
+                    .filter(withCustomTemplates())
+                    .body(authData)
+                    .contentType(JSON)
+                    .log().uri()
+                    .log().all()
+                    .when()
+                    .post("/register")
+                    .then()
+                    .log().status()
+                    .log().body()
+                    .log().all()
+                    .statusCode(200)
+                    .extract().as(ResponseBodyModel.class);
+        });
+        step("Check response", () -> {
+            assertEquals(4,response.getId());
+            assertEquals("QpwL5tke4Pnpja7X4",response.getToken());
+        });
     }
 
     @DisplayName("Не успешная регистрация пользователя")
