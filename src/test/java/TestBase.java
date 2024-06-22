57bc3606fe0c0c1ebcc3e478e124950d37e4fc32
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-06-22 20:48:04 +0300

    update testbase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,5 +5,6 @@ public class TestBase {
     @BeforeAll
    public static void beforeAll(){
         RestAssured.baseURI = "https://reqres.in/";
+        RestAssured.basePath = "api/";
     }
 }
--- src/test/java/models/PathUpdateRequestModel.java
+++ src/test/java/models/PathUpdateRequestModel.java
@@ -1,6 +1,5 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
